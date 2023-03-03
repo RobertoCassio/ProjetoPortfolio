81870a91e569d77fac41c0f220ffa6c2625fd0dc
RobertoCassio <rcslino@gmail.com>
2023-03-02 22:48:53 -0300

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const target = document.querySelectorAll('[data-animation]');  //Seleciono todos elementos com data-animation
+const animationClass = 'animate';
+
+function animationScroll(){
+    const windowTop = window.innerHeight*0.75; //Aqui eu pego a altura da janela no geral
+  
+    target.forEach(function(element){
+        const elTop = element.getBoundingClientRect().top; //Aqui pego a posição do objeto em relação ao local atual
+
+        const visibleSection = (elTop - windowTop) < 0; //Se um menos o outro for < 0, então estamos em cima do elemento alvo
+
+        if(visibleSection){ //Sendo assim, podemos chamar a função de animation
+            element.classList.add(animationClass);
+        }
+        else{
+            element.classList.remove(animationClass);
+        }
+        console.log("Elemento: " + elTop);
+        console.log("Página: "+ windowTop);
+    })
+
+}
+
+window.addEventListener('scroll', function(){        //Toda vez que o usuário scrollar chama o animationScroll
+    animationScroll();
+})
